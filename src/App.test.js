3d381c1098edbd1361b91798940b7f7cb9395b0d
edwardyqliu <114708188+edwardyqliu@users.noreply.github.com>
2023-03-16 19:16:37 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,19 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 
 // import your story from App.stories.js
-
+import App from './App.stories';
 
 // Add your tests here
 // See https://storybook.js.org/docs/react/writing-tests/importing-stories-in-tests#example-with-testing-library
+test('Checks if inital state is set to click me', () => {
+    const {getText} = render(<button label = "aButton"/>);
+    //console.log(screen.getByRole("button").textContent);
+    expect(screen.getByRole("button").textContent == "click me");
+});
+
+test('Checks if state after one click is thanks!', () => {
+    const {getText} = render(<button label = "aButton"/>);
+    //console.log(screen.getByRole("button").textContent);
+    fireEvent.click(screen.getByRole("button"))
+    expect(screen.getByRole("button").textContent == "click me");
+});
