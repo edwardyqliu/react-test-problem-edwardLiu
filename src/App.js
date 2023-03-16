7f1b1c9168aa8e5911ff4cebecd3eec273ce090a
edwardyqliu <114708188+edwardyqliu@users.noreply.github.com>
2023-03-16 19:15:34 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      textClicked: false
     };
+
   }
 
   //handler goes here
+  changeText(){
+    this.setState(state =>({
+      textClicked: !state.textClicked
+    }));
+  }
 
   render() {
+    const {text} = this.state
+
     return (
       <div>
         <p>Greetings!</p>
-        <p>Button goes here</p>
+          <button label = "aButton" onClick = {() => {this.changeText(text)}}>
+            {this.state.textClicked ? "thanks!" : "click me"}
+          </button>
       </div>
     );
   }
